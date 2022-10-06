e5b09ffdddb999263e9ef5b3712fdcf291b4f180
Dylan-VT <dylan.lawrence.vt@gmail.com>
2022-10-06 11:16:16 -0400

    model testing

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+class User(BaseModel):
+    name: str
+    birthday: str
+    friends: List[str]
 
 app = FastAPI()
+
+
 
 
 @app.get("/")
@@ -10,6 +18,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/getuser/{userid}", response_model = user)
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
