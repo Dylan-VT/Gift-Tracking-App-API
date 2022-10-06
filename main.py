fb63bf184e0c0899b5adb432fd11ae8f9023dbea
Dylan-VT <dylan.lawrence.vt@gmail.com>
2022-10-06 11:17:00 -0400

    model testing

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/getuser/{userid}", response_model = user)
+@app.get("/getuser/{userid}", response_model = User)
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
