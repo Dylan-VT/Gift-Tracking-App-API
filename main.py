b5d1c710d790dfe1dc33c0ea913e1f1aaad2491b
Dylan-VT <dylan.lawrence.vt@gmail.com>
2022-10-18 18:33:46 -0400

    changing to AWS

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+# cors setup
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class User(BaseModel):
     name: str
@@ -20,4 +37,8 @@ def read_root():
 
 @app.get("/getuser/{userid}", response_model = User)
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"user": "test"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
