1afc7995748f870cfb955c12589ebab28ecba17c
Dylan-VT <dylan.lawrence.vt@gmail.com>
2022-10-19 14:53:56 -0400

    Started work on get user query

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from typing import Union
+import dis
+from typing import Optional, Union
+from click import echo
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+import sqlalchemy
+#load in db connection string
+load_dotenv()
+
+
+#create engine
+
+engine = sqlalchemy.create_engine(os.environ['DB_URL'], echo = True)
 
 
 app = FastAPI()
@@ -21,9 +33,11 @@ app.add_middleware(
 
 
 class User(BaseModel):
-    name: str
+    user_id: int
+    username: str
+    display_name: str
     birthday: str
-    friends: List[str]
+    friends: Optional[List[str]]
 
 app = FastAPI()
 
@@ -32,13 +46,31 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Traveler"}
+    #connect to db
+    with engine.connect() as conn:
+        result = conn.execute(sqlalchemy.text('SELECT * FROM public.users;'))
+        for r in result:
+            print(r)   
+    return {"Hi": "Bye"}
 
 
-@app.get("/getuser/{userid}", response_model = User)
-def read_item(userid: int, q: Union[str, None] = None):
-    return {"user": "test"}
+@app.get("/getuser/{username}")
+def read_item(username: str, q: Union[str, None] = None):
+    #connect to db
+    with engine.connect() as conn:
+        #execute query
+        result = conn.execute(sqlalchemy.text(f'SELECT username, user_id, display_name, birthday, friends FROM public.users WHERE "username" = \'{username}\';'))
+        r = result.fetchone()
+        
+        #verify r exists
+        if not r:
+            return 501
+        
+        found_user = User(user_id = r[1], username = r[0], display_name = r[2], birthday = str(r[3]), friends = r[4])
+        
+        return found_user
+    
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
