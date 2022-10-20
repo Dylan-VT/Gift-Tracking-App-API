3ce9f5018f17e16e06f3a09c2f90465633ece474
Dylan-VT <dylan.lawrence.vt@gmail.com>
2022-10-20 17:33:46 -0400

    .gitignore modifications

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from cgitb import reset
-from typing import Union
-from unittest import result
-from fastapi import FastAPI
-import uvicorn
-from fastapi.middleware.cors import CORSMiddleware
+"""
+root file for gift tracker api
+will pull together end points and models
+"""
 import os
-from dotenv import load_dotenv
-from models import user_models
-from metadata import users
+import psycopg2
+import uvicorn
 import sqlalchemy
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+#custom modules
+from metadata import users
+from models import user_models
+
 
 
 #load in db connection string
@@ -34,20 +39,22 @@ app.add_middleware(
 )
 
 
-
-
 app = FastAPI()
-
-
 
 
 @app.get("/")
 def read_root():
+    """
+    does nothing..... yet
+    """
     return {"Hello": "World"}
 
 
 @app.get("/getuser/{username}")
-def read_item(username: str, q: Union[str, None] = None):
+def read_item(username: str):
+    """
+    Gets a user from the db that matches the username
+    """
     #create select statement
     select = users.select().where(users.c.username == username)
     #get result, fetch first item
@@ -55,32 +62,40 @@ def read_item(username: str, q: Union[str, None] = None):
     if result is None:
         print("Error fetching user")
         return 500
-    
+
     return result
-    
+
 @app.post("/createuser")
 def create_item(user: user_models.CreateUser):
+    """
+    Creates a new user and adds them to the DB
+    """
     #insert statement to create new user
-    insert = users.insert().values(username = user.username, display_name = user.display_name, birthday = user.birthday)
+    insert = users.insert().values(username = user.username,
+                                   display_name = user.display_name,
+                                   birthday = user.birthday)
     #make query
     try:
-        result = make_simple_query(insert, engine)
-    except:
+        make_simple_query(insert, engine)
+    except psycopg2.Error:
         print("Exception encountered at /createuser")
         return 500
-    
+
     return 200
 
 
-#simple function that takes a sql statement and engine and returns the result of the query
-def make_simple_query(sql_statement, engine):
+
+def make_simple_query(sql_statement, _e):
+    """
+    simple function that takes a sql statement and engine and returns the result of the query
+    """
     #open connection
-    with engine.connect() as conn:
+    with _e.connect() as conn:
         #make query
         result = conn.execute(sql_statement)
         return result
 
-        
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
